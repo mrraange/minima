5e22bfc8dd62f3ef26bbf0da5b944eff169526be
Range <100018176+mrraange@users.noreply.github.com>
2022-09-01 21:17:01 +0300

    Add files via upload

---
+++ minima_cron_minima_9001.sh
@@ -0,0 +1,33 @@
+sudo systemctl restart minima_9001
+sleep 50
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+sleep 30
+curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
