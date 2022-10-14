7639045c046fcf63736a110815939063e048206a
Range <100018176+mrraange@users.noreply.github.com>
2022-10-14 11:09:54 +0300

    Update minima_cron_minima_9001.sh

--- minima_cron_minima_9001.sh
+++ minima_cron_minima_9001.sh
@@ -1,33 +1,7 @@
 sudo systemctl restart minima_9001
 sleep 50
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+curl -s 127.0.0.1:9005/incentivecash%20uid:ваш UID
 sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+curl -s 127.0.0.1:9005/incentivecash%20uid:ваш UID
 sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
-sleep 30
-curl -s 127.0.0.1:9002/incentivecash%20uid:ваш UID
+curl -s 127.0.0.1:9005/incentivecash%20uid:ваш UID
